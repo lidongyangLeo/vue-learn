4c560c329aa9b9102e3b865a206134f935d17ba4
lidongyang <752860629@qq.com>
2023-12-08 21:53:48 +0800

    chore: add commitlint and husky

---
+++ commitlint.config.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  ignores: [(commit) => commit.includes('init')],
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'build',
+        'revert',
+        'merge',
+        'upgrade',
+      ],
+    ],
+    'type-case': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 108],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'subject-case': [0],
+  },
+}
