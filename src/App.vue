418688b0c982423175a182081f65ba71b6ca2868
lidongyang <752860629@qq.com>
2023-12-09 22:10:09 +0800

    chore: add element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
-    <h1>APP</h1>
+    <el-button type="primary" size="default" :icon="Plus">button</el-button>
+    <el-button type="success" size="large" :icon="Edit">
+      success button
+    </el-button>
+    <el-button type="danger" size="small" :icon="Delete">
+      delete button
+    </el-button>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Delete, Edit, Plus } from '@element-plus/icons-vue'
+</script>
 
 <style scoped></style>
